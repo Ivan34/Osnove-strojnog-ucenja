4475a361b9eb379dcda0267f90c0a09820bf2797
Ivan34 <42832503+Ivan34@users.noreply.github.com>
2026-03-10 22:00:28 +0100

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-def total_euro(hours, pay):
-    return hours * pay
-
-
-print("Unesite radne sate:")
-hoursWorked = int(input())
-print("Unesite satnicu:")
-hourlyPay = float(input())
-print("Ukupno", total_euro(hoursWorked, hourlyPay), "eura")
